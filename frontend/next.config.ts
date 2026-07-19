fff01f7f02f72cbda2dd1202f6da2ea32568ded5
Ujjawal saini <ujjawalsaini1@gmail.com>
2026-07-20 01:24:26 +0530

    allow cloudinary images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**/image/upload/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
